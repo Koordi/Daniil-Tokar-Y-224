b88c5d9f89b49e9e2d828573604d236b4b298083
Daniil Tokar <koordinaltokar@gmail.com>
2022-11-07 21:12:48 +0300

    new

---
+++ Algoritm/8 Practice/Code/14 variant 1.py
@@ -0,0 +1,18 @@
+m,n=map(int,input("Введите интервал:").split())
+kd=0
+kd2=0
+l=[]
+for i in range(m,n+1):
+    for j in range(2,i):
+        if i%j==0:
+            kd2+=1
+    if kd2>kd:
+        kd=kd2
+    kd2=0
+for i in range(m,n+1):
+    for j in range(2,i):
+        if i%j==0:
+            kd2+=1
+    if kd2==kd:
+        print(i, end=' ')
+    kd2=0    
---
+++ Algoritm/9 Practice/Code/14 variant 2.py
@@ -0,0 +1,44 @@
+n=int(input('Введите n: '))
+k=0
+j=0
+p=0
+lst = [0] * n
+for i in range(n):
+    lst[i] = [0] * n
+while k!=n*n: 
+    for i in range(p,n):       
+        if lst[j][i]==0:
+            k+=1
+            lst[j][i]=k
+        else:
+            i-=1
+            break
+    p=j+1
+    for j in range(p,n):
+        if lst[j][i]==0:
+            k+=1
+            lst[j][i]=k
+        else:
+            j-=1
+            break
+    p=i-1
+    for i in range(p,-1,-1):       
+        if lst[j][i]==0:
+            k+=1
+            lst[j][i]=k
+        else:
+            i+=1
+            break
+    p=j-1
+    for j in range(p,-1,-1):
+        if lst[j][i]==0:
+            k+=1
+            lst[j][i]=k
+        else:
+            j+=1
+            break
+    p=i+1
+for j in range(n):
+    for i in range(n):
+        print(lst[j][i],end=' ')
+    print()
